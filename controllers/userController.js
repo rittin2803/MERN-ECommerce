b9c1907920a28d10edfb54207da5226e1b4b7d44
rittin2803 <rittin2803@gmail.com>
2024-06-30 23:23:06 +0530

    User model and Register user route

---
+++ controllers/userController.js
@@ -0,0 +1,17 @@
+const User = require("../models/userModel");
+
+const createUser = async (req, res) => {
+    const email = req.body.email;
+    const findUser = await User.findOne({ email : email });
+    
+    if(!findUser){
+        // Create new User if not exists
+        const newUser = await User.create(req.body);
+        res.json(newUser);
+    }else{
+        // User already exists
+        res.json({ message: "User already exists", success: false});
+    }
+};
+
+module.exports = { createUser };
--- index.js
+++ index.js
@@ -2,20 +2,18 @@ const express = require("express");
 const dotenv = require("dotenv");
 
 const dbConnect = require("./config/dbConnect");
+const authRouter = require("./routes/authRoute");
 
 dotenv.config();
 
 const app = express();
-
 const PORT = process.env.PORT || 4000;
 
 dbConnect();
 
 app.use(express.json());
 
-app.use("/", (req,res) => {
-    res.send("Hello");
-})
+app.use("/api/v1/user", authRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
